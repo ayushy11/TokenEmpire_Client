3054694232eb6f0cd2f14da7c6c4976eb8225faa
AYUSH YADAV <ayushexx@gmail.com>
2022-01-05 14:32:45 +0530

    feat: welcome section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,9 +12,11 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
+
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
+
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchanges", "Tutorials", "Wallets"].map((item, index) => (
           <NavbarItem key={item + index} title={item} />
@@ -23,7 +25,9 @@ const Navbar = () => {
           Login
         </li>
       </ul>
+
       <div className="flex relative">
+
         {toggleMenu ? (
           <AiOutlineClose
             fontSize={28}
@@ -37,6 +41,7 @@ const Navbar = () => {
             onClick={() => setToggleMenu(true)}
           />
         )}
+
         {toggleMenu && (
           <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
             <li className="text-xl w-full my-2">
@@ -53,7 +58,9 @@ const Navbar = () => {
             )}
           </ul>
         )}
+        
       </div>
+
     </nav>
   );
 };
