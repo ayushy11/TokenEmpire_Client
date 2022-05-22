f0e1f95f7a6e5cc591fef6d92f5f6cd3a5d9bf32
AYUSH YADAV <ayushexx@gmail.com>
2022-05-22 18:53:16 +0530

    feat: footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,41 @@
+import logo from "../../images/token.png";
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} alt="logo" className="w-32" />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Market
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Exchange
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Tutorials
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Wallets
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center flex-col mt-5">
+        <p className="text-white text-sm text-center">Come join us</p>
+        <p className="text-white text-sm text-center">info@tokenempire.com</p>
+      </div>
+
+      <div className="w-full h-[0.25px] bg-gray-400 mt-5 sm:w-[90%]" />
+
+      <div className="flex w-full justify-between items-center mt-3 sm:w-[90%]">
+        <p className="text-white text-left">@tokenempire2022</p>
+        <p className="text-white text-right">All rights reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
