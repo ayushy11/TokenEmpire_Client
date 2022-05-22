d32ae90b798e3428c8837953c820e491e4f85125
AYUSH YADAV <ayushexx@gmail.com>
2022-05-22 22:44:56 +0530

    fix: env

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Services from "./components/Services/Services";
 import Transactions from "./components/Transactions/Transactions";
-import Loader from "./components/Loader";
 import Welcome from "./components/Welcome";
 
 const App = () => {
